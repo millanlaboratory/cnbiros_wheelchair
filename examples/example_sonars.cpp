c4c278ee3b5c29c8841400a4f76d4040ccceeb7c
Luca Tonin <luca.tonin@gmail.com>
2017-12-01 13:14:02 +0100

    number of sonar as argument

--- examples/example_sonars.cpp
+++ examples/example_sonars.cpp
@@ -4,23 +4,26 @@
 #include <iostream>
 
 void usage( char *x ) {
-	printf( "usage: %s /dev/ttyUSB0\n", x );
+	printf( "usage: %s /dev/ttyUSB0 5\n", x );
 }
 
 int main( int argc, char *argv[] ) 
 {
 
-	int N_SONAR = 2;
+	int N_SONAR;
 	cnbiros::wheelchair::SonarThread *sr;
 	
-	if ( argc != 2 ) {
-		printf( "please provide exactly 1 argument\n" );
+	if ( argc != 3 ) {
+		printf( "please provide exactly 2 arguments (port and number of sensors)\n" );
 		usage( argv[0] );
 		return EINVAL;
 	}
+	N_SONAR = std::stoi(std::string(argv[2]));
 
 	sr = new cnbiros::wheelchair::SonarThread(argv[1], N_SONAR);
 				
+	printf("Checking %d sensors on port %s\n", N_SONAR, argv[1]);
+
 	int range[N_SONAR];
 		
 	while ( 1 ) { 
